dce3fc8685e9f8a51a84a5c9d40177ee19128fa0
Daniel Ryan <nz.danielryan@gmail.com>
2018-04-16 23:10:09 +1200

    wip

---
+++ app/README.md
@@ -0,0 +1,7 @@
+# Scoreo
+
+This is the repo for for the Scoreo point tracking app.
+
+Scoreo is an experimental Angular NativeScript mobile app. 
+
+Currently, only android will be targeted.
--- app/platforms/android/app/build/intermediates/assets/debug/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "my-app",
-  templateUrl: "app.component.xml"
-})
-export class AppComponent {
-  // Your TypeScript logic goes here
-}
